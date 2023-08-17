158e18593122d4f0b3286ec4db425bcc86580994
TheFurious77 <shreyanshsingh0901@gmail.com>
2023-08-17 17:51:29 +0530

    I have made some changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+// import About from './components/About';
+import Alert from './components/Alert';
+import React,{useState} from 'react';
+// import {
+//   BrowserRouter as Router,
+//  Routes,
+//   Route
+// } from "react-router-dom";
+
 
 function App() {
+  const [mode,setmode]=useState("light");
+  const [alert,setalert]=useState(null);
+
+  const showAlert = (message,type) =>{
+    setalert({
+      msg:message,
+      type:type
+    })
+
+    setTimeout(() => {
+      setalert(null)
+    },777 );
+  }
+  const toggle =() =>{
+    if(mode==="light"){
+      setmode("dark")
+      document.body.style.backgroundColor='#042743'
+      showAlert("Dark Mode has Enabled","success")
+      document.title="DARK_MODE"
+    }
+    else{
+      setmode("light")
+      document.body.style.backgroundColor='white'
+      showAlert("Light Mode has Enabled","success")
+      document.title="LIGHT_MODE"
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    {/* <Router> */}
+    <Navbar title="Utils" aboutText="Among Us" mode={mode} toggle={toggle}/>
+    <Alert alert={alert}/>
+    <div className="container my-3">
+
+    /<TextForm showAlert={showAlert} heading="Write What you Believe is True" mode={mode}/>
+    {/* <Routes>
+      <Route path="/about" element={<About mode={mode}/>}/>
+      {/* <Route path='/' element={<TextForm showAlert={showAlert} heading="Write What you Believe is True" mode={mode}/>}/> */}
+      {/* <Route path='/' element={<TextForm showAlert={showAlert} heading="Write What you Believe is True" mode={mode}/>}/> */}
+    {/* </Routes> */}
     </div>
+    {/* </Router> */}
+    
+    
+        </>
   );
 }
 
---
+++ src/components/TextForm.js
@@ -0,0 +1,41 @@
+import React, {useState} from 'react'
+
+export default function TextForm(props) {
+  const handleUpClick=()=>{
+  //tconsole.log("Who Am I" + text);
+  let newtext=text.toUpperCase();
+  setText(newtext)
+  props.showAlert("UpperCase has Been Converted","success")
+  }
+  const handleUpClear=()=>{
+    let newtext="";
+    setText(newtext)
+    props.showAlert("Clear","success")
+    }
+
+  const handleOnChange = (event) =>{
+   // console.log("On change");
+    setText(event.target.value)
+  }
+  const [text,setText]=useState('');
+  return (
+    <>
+      <div className='container' style={{color: props.mode==="dark"?"white":"#042743"}}>
+    <h1>{props.heading}</h1>
+        <div className="mb-3">
+    <textarea className ="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==="dark"?"grey":"white",color:props.mode==="dark"?"white":"#042743"}} id="myBox" row="8"></textarea>
+    </div>
+    <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to uppercase</button>
+    <button className="btn btn-primary mx-2" onClick={handleUpClear}>Clear</button>
+
+    </div>
+    <div className="container my-3" style={{color: props.mode==="dark"?"white":"#042743"}}>
+      <h1>Word tracker</h1>
+      <p> Words {text.split(" ").length} character {text.length}</p>
+      <p>Time required to read {text.split(" ").length*4.8} seconds</p>
+      <h2>Preview</h2>
+      <p>{text.length>0?text:"Enter The Text"}</p>
+    </div>
+    </>
+  )
+}
